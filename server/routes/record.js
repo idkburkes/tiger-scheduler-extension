8a5921b78af5a9b98db44679bc33007bcef3d8c7
Isaiah <idkburkes@gmail.com>
2021-11-18 21:42:41 -0600

    Added ability to leave reviews and persist them in NoSQL database

--- server/routes/record.js
+++ server/routes/record.js
@@ -14,9 +14,9 @@ const ObjectId = require("mongodb").ObjectId;
 
 // This section will help you get a list of all the records.
 recordRoutes.route("/record").get(function (req, res) {
-  let db_connect = dbo.getDb("employees");
+  let db_connect = dbo.getDb("tiger-scheduler-extension");
   db_connect
-    .collection("records")
+    .collection("reviews")
     .find({})
     .toArray(function (err, result) {
       if (err) throw err;
@@ -51,23 +51,62 @@ recordRoutes.route("/record/add").post(function (req, response) {
 });
 
 // This section will help you update a record by id.
-recordRoutes.route("/update/:id").post(function (req, response) {
+recordRoutes.route("/update/:name").post(function (req, response) {
   let db_connect = dbo.getDb();
-  let myquery = { _id: ObjectId( req.params.id )};
-  let newvalues = {
-    $set: {
-      person_name: req.body.person_name,
-      person_position: req.body.person_position,
-      person_level: req.body.person_level,
-    },
-  };
+  let myquery = { "name": req.body.review.name };
+
+  let newReview = {
+    overall: req.body.review.overall,
+    difficulty: req.body.review.difficulty,
+    comment: req.body.review.comment,
+    would_take_again: req.body.review.would_take_again
+  }
+
+  let document = {
+    custom_reviews: [],
+    difficulty_total: 0,
+    overall_total: 0.0,
+    review_count: 0,
+    would_take_again: 0.0
+  }
+
+  // Find previous values of existing document
   db_connect
-    .collection("records")
-    .updateOne(myquery, newvalues, function (err, res) {
-      if (err) throw err;
-      console.log("1 document updated");
-      response.json(res);
-    });
+  .collection("reviews")
+  .findOne(myquery, function (err, result) {
+    if (err) throw err;
+  
+    //Update all review totals including this new review
+    document.review_count = result.review_count + 1;
+    document.difficulty_total = (result.difficulty_total * 1.0) + (newReview.difficulty * 1.0);
+    document.overall_total = (result.overall_total * 1.0) + (newReview.overall * 1.0);
+    var take_again_count = result.would_take_again * result.review_count;
+    // plus sign and toFixed() handles converting perfect to 2 decimal places
+    var new_take_again_percent = +((take_again_count + 1)/(result.review_count + 1)).toFixed(2);
+    document.would_take_again = new_take_again_percent;
+
+    // Update values for this document
+    let newvalues = {
+      $set: {
+        review_count: document.review_count,
+        would_take_again: document.would_take_again,
+        difficulty_total: document.difficulty_total,
+        overall_total: document.overall_total
+      },
+      $push: {
+        custom_reviews: newReview
+      }
+    };
+    db_connect
+      .collection("reviews")
+      .updateOne(myquery, newvalues, function (err, res) {
+        if (err) throw err;
+        console.log("1 document updated");
+        response.json(res);
+      });
+  });
+
+
 });
 
 // This section will help you delete a record
